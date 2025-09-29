f6c59e8928e4af9c89564bc522fd596d511bb386
MD ABDUR RAHAMAN <abdurrahaman444.spy.com>
2025-09-29 16:45:06 +0600

    project mastering

---
+++ src/components/client.vue
@@ -0,0 +1,42 @@
+<template>
+    		
+		<!-- Start clients -->
+		<div class="clients overlay">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12 col-md-12 col-12">
+						<div class="owl-carousel clients-slider">
+							<div class="single-clients">
+								<img src="../assets/img/client1.png" alt="#">
+							</div>
+							<div class="single-clients">
+								<img src="../assets/img/client2.png" alt="#">
+							</div>
+							<div class="single-clients">
+								<img src="../assets/img/client3.png" alt="#">
+							</div>
+							<div class="single-clients">
+								<img src="../assets/img/client4.png" alt="#">
+							</div>
+							<div class="single-clients">
+								<img src="../assets/img/client5.png" alt="#">
+							</div>
+							<div class="single-clients">
+								<img src="../assets/img/client1.png" alt="#">
+							</div>
+							<div class="single-clients">
+								<img src="../assets/img/client2.png" alt="#">
+							</div>
+							<div class="single-clients">
+								<img src="../assets/img/client3.png" alt="#">
+							</div>
+							<div class="single-clients">
+								<img src="../assets/img/client4.png" alt="#">
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!--/Ens clients -->
+</template>
